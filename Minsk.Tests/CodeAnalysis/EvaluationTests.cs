f1afe9c79caa44593407cd468d33c04ace8a2948
Tyler Tesch <tytesch@gmail.com>
2019-02-14 20:52:21 -0600

    Make error messages consistent

--- Minsk.Tests/CodeAnalysis/EvaluationTests.cs
+++ Minsk.Tests/CodeAnalysis/EvaluationTests.cs
@@ -7,6 +7,7 @@ using Xunit;
 
 namespace Minsk.Tests.CodeAnalysis
 {
+
     public class EvaluationTests
     {
         [Theory]
@@ -32,7 +33,117 @@ namespace Minsk.Tests.CodeAnalysis
         [InlineData("!true", false)]
         [InlineData("!false", true)]
         [InlineData("{ var a = 0 (a = 10) * a }", 100)]
-        public void SyntaxFact_GetText_RoundTrips(string text, object expectedValue)
+        public void Evaluator_Computes_CorrectValues(string text, object expectedValue)
+        {
+            AssertValue(text, expectedValue);
+        }
+
+        [Fact]
+        public void Evaluator_VariableDeclaration_Reports_Redeclaration()
+        {
+            var text = @"
+                {
+                    var x = 10
+                    var y = 100
+
+                    {
+                        var x = 10
+                    }
+
+                    var [x] = 5
+                }
+            ";
+
+            var diagnostics = @"
+                Variable 'x' is already declared.
+            ";
+
+            AssertDiagnostiscs(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Name_Reports_Undefined()
+        {
+            var text = @"[x] * 10";
+
+            var diagnostics = @"
+                Variable 'x' doesn't exist.
+            ";
+
+            AssertDiagnostiscs(text, diagnostics);
+        }
+        
+        [Fact]
+        public void Evaluator_Assigned_Reports_Undefined()
+        {
+            var text = @"[x] = 10";
+
+            var diagnostics = @"
+                Variable 'x' doesn't exist.
+            ";
+
+            AssertDiagnostiscs(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Assigned_Reports_CannotAssign()
+        {
+            var text = @"
+                {
+                    let x = 10
+                    x [=] 0
+                }
+            ";
+
+            var diagnostics = @"
+                Variable 'x' is read-only and cannot be assigned to.
+            ";
+
+            AssertDiagnostiscs(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Assigned_Reports_CannotConvert()
+        {
+            var text = @"
+                {
+                    var x = 10
+                    x = [true]
+                }
+            ";
+
+            var diagnostics = @"
+                Cannot convert type 'System.Boolean' to 'System.Int32'.
+            ";
+
+            AssertDiagnostiscs(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Unary_Reports_Undefined()
+        {
+            var text = @"[+]true";
+
+            var diagnostics = @"
+                Unary operator '+' is not defined for type 'System.Boolean'.
+            ";
+
+            AssertDiagnostiscs(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Binary_Reports_Undefined()
+        {
+            var text = @"10 [*] false";
+
+            var diagnostics = @"
+                Binary operator '*' is not defined for types 'System.Int32' and 'System.Boolean'.
+            ";
+
+            AssertDiagnostiscs(text, diagnostics);
+        }
+
+        private static void AssertValue(string text, object expectedValue)
         {
             var syntaxTree = SyntaxTree.Parse(text);
             var compilation = new Compilation(syntaxTree);
@@ -41,6 +152,33 @@ namespace Minsk.Tests.CodeAnalysis
 
             Assert.Empty(result.Diagnostics);
             Assert.Equal(expectedValue, result.Value);
+        }
+
+        private void AssertDiagnostiscs(string text, string diagnosticText)
+        {
+            var annotatedText = AnnotatedText.Parse(text);
+            var syntaxTree = SyntaxTree.Parse(annotatedText.Text);
+            var compilation = new Compilation(syntaxTree);
+            var result = compilation.Evaluate(new Dictionary<VariableSymbol, object>());
+
+            var expectedDiagnostics = AnnotatedText.UnindentLines(diagnosticText);
+
+            if (annotatedText.Spans.Length != expectedDiagnostics.Length)
+                throw new Exception("ERROR: Must mark as many spans as there are expected diagnostics");
+
+            Assert.Equal(expectedDiagnostics.Length, result.Diagnostics.Length);
+
+            for (var i = 0; i < expectedDiagnostics.Length; i++) 
+            {
+                var expectedMessage = expectedDiagnostics[i];
+                var actualMessage = result.Diagnostics[i].Message;
+
+                Assert.Equal(expectedMessage, actualMessage);
+
+                var expectedSpan = annotatedText.Spans[i];
+                var actualSpan = result.Diagnostics[i].Span;
+                Assert.Equal(expectedSpan, actualSpan);
+            }
         }
     }
 }
