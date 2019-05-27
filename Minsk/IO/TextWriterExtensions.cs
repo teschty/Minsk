11b0002cb30fb9abb99ec261cf2a79ccd30d2116
Tyler Tesch <tytesch@gmail.com>
2019-05-27 15:55:12 -0500

    Pretty print BoundNodes in a more compact fashion

---
+++ Minsk/IO/TextWriterExtensions.cs
@@ -0,0 +1,64 @@
+using System;
+using System.CodeDom.Compiler;
+using System.IO;
+
+namespace Minsk.IO
+{
+    internal static class TextWriterExtensions
+    {
+        public static bool IsConsoleOut(this TextWriter writer)
+        {
+            if (writer == Console.Out)
+                return true;
+
+            if (writer is IndentedTextWriter iw && iw.InnerWriter.IsConsoleOut())
+                return true;
+
+            return false;
+        }
+
+        public static void SetForeground(this TextWriter writer, ConsoleColor color)
+        {
+            if (writer.IsConsoleOut())
+                Console.ForegroundColor = color;
+        }
+
+        public static void ResetColor(this TextWriter writer)
+        {
+            if (writer.IsConsoleOut())
+                Console.ResetColor();
+        }
+
+        public static void WriteWithColor(this TextWriter writer, string text, ConsoleColor color)
+        {
+            writer.SetForeground(color);
+            writer.Write(text);
+            writer.ResetColor();
+        }
+
+        public static void WriteKeyword(this TextWriter writer, string text)
+        {
+            writer.WriteWithColor(text, ConsoleColor.Blue);
+        }
+
+        public static void WriteIdentifier(this TextWriter writer, string text)
+        {
+            writer.WriteWithColor(text, ConsoleColor.DarkYellow);
+        }
+
+        public static void WriteNumber(this TextWriter writer, string text)
+        {
+            writer.WriteWithColor(text, ConsoleColor.Cyan);
+        }
+
+        public static void WriteString(this TextWriter writer, string text)
+        {
+            writer.WriteWithColor(text, ConsoleColor.Magenta);
+        }
+
+        public static void WritePunctuation(this TextWriter writer, string text)
+        {
+            writer.WriteWithColor(text, ConsoleColor.DarkGray);
+        }
+    }
+}
